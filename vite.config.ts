4ba20997c612c0d5907d6c27e35a0449ab39ba92
Cory Fitzpatrick <fitzmx6@gmail.com>
2025-11-01 10:07:53 -0400

    Fix base path for GitHub Pages subdirectory

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
     plugins: [react()],
-    base: '/',
+    base: '/reactportfolio/',
     server: {
         port: 3000,
         strictPort: true,
